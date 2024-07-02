324d22696511ddfa3e2157c9c885b04809a44529
Sansrita <sansritasaha@gmail.com>
2024-07-02 22:34:36 +0530

    Bag: Css fix

--- bag.js
+++ bag.js
@@ -1,147 +1,146 @@
 let bagitemObject;
 const CONVENIENCE_FEES = 99;
-const items = [
-    {
-      id: "001",
-      image: "images/1.jpg",
-      company: "Carlton London",
-      item_name: "Rhodium-Plated CZ Floral Studs",
-      original_price: 1045,
-      current_price: 606,
-      discount_percentage: 42,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.5,
-        count: 1400,
-      },
+const items = [{
+        id: "001",
+        image: "images/1.jpg",
+        company: "Carlton London",
+        item_name: "Rhodium-Plated CZ Floral Studs",
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
     },
     {
-      id: "002",
-      image: "images/2.jpg",
-      company: "CUKOO",
-      item_name: "Women Padded Halter Neck Swimming Dress",
-      original_price: 2599,
-      current_price: 1507,
-      discount_percentage: 42,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.3,
-        count: 24,
-      },
+        id: "002",
+        image: "images/2.jpg",
+        company: "CUKOO",
+        item_name: "Women Padded Halter Neck Swimming Dress",
+        original_price: 2599,
+        current_price: 1507,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
     },
     {
-      id: "003",
-      image: "images/3.jpg",
-      company: "NUEVOSDAMAS",
-      item_name: "Women Red & White Printed A-Line Knee-Length Skirts",
-      original_price: 1599,
-      current_price: 495,
-      discount_percentage: 69,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.1,
-        count: 249,
-      },
+        id: "003",
+        image: "images/3.jpg",
+        company: "NUEVOSDAMAS",
+        item_name: "Women Red & White Printed A-Line Knee-Length Skirts",
+        original_price: 1599,
+        current_price: 495,
+        discount_percentage: 69,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 4.1,
+            count: 249,
+        },
     },
     {
-      id: "004",
-      image: "images/4.jpg",
-      company: "ADIDAS",
-      item_name: "Indian Cricket ODI Jersey",
-      original_price: 999,
-      current_price: 999,
-      discount_percentage: 0,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 5.0,
-        count: 10,
-      },
+        id: "004",
+        image: "images/4.jpg",
+        company: "ADIDAS",
+        item_name: "Indian Cricket ODI Jersey",
+        original_price: 999,
+        current_price: 999,
+        discount_percentage: 0,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 5.0,
+            count: 10,
+        },
     },
     {
-      id: "005",
-      image: "images/5.jpg",
-      company: "Roadster",
-      item_name: "Pure Cotton T-shirt",
-      original_price: 1399,
-      current_price: 489,
-      discount_percentage: 65,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.2,
-        count: 3500,
-      },
+        id: "005",
+        image: "images/5.jpg",
+        company: "Roadster",
+        item_name: "Pure Cotton T-shirt",
+        original_price: 1399,
+        current_price: 489,
+        discount_percentage: 65,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 4.2,
+            count: 3500,
+        },
     },
     {
-      id: "006",
-      image: "images/6.jpg",
-      company: "Nike",
-      item_name: "Men ReactX Running Shoes",
-      original_price: 14995,
-      current_price: 14995,
-      discount_percentage: 0,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 0.0,
-        count: 0,
-      },
+        id: "006",
+        image: "images/6.jpg",
+        company: "Nike",
+        item_name: "Men ReactX Running Shoes",
+        original_price: 14995,
+        current_price: 14995,
+        discount_percentage: 0,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 0.0,
+            count: 0,
+        },
     },
     {
-      id: "007",
-      image: "images/7.jpg",
-      company: "The Indian Garage Co",
-      item_name: "Men Slim Fit Regular Shorts",
-      original_price: 1599,
-      current_price: 639,
-      discount_percentage: 60,
-      rating: {
-        stars: 4.2,
-        count: 388,
-      },
+        id: "007",
+        image: "images/7.jpg",
+        company: "The Indian Garage Co",
+        item_name: "Men Slim Fit Regular Shorts",
+        original_price: 1599,
+        current_price: 639,
+        discount_percentage: 60,
+        rating: {
+            stars: 4.2,
+            count: 388,
+        },
     },
     {
-      id: "008",
-      image: "images/8.jpg",
-      company: "Nivea",
-      item_name: "Men Fresh Deodrant 150ml",
-      original_price: 285,
-      current_price: 142,
-      discount_percentage: 50,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.2,
-        count: 5200,
-      },
+        id: "008",
+        image: "images/8.jpg",
+        company: "Nivea",
+        item_name: "Men Fresh Deodrant 150ml",
+        original_price: 285,
+        current_price: 142,
+        discount_percentage: 50,
+        return_period: 14,
+        delivery_date: "10 Oct 2023",
+        rating: {
+            stars: 4.2,
+            count: 5200,
+        },
     },
-  ];
+];
 onLoad();
 
 function onLoad() {
-  loadBagItems()
-  displayBagItems();
-  displayBagSum();
+    loadBagItems()
+    displayBagItems();
+    displayBagSum();
 }
 
-function displayBagSum(){
-    let totalItem=0;
-    let totalMRP=0;
-    let totalDiscount=0;
-    
+function displayBagSum() {
+    let totalItem = 0;
+    let totalMRP = 0;
+    let totalDiscount = 0;
+
     bagitemObject.forEach(bagItem => {
         totalMRP += bagItem.original_price;
         totalDiscount += bagItem.original_price - bagItem.current_price;
-      });
-    
-      let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
-      
+    });
+
+    let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+
     let bagSumele = document.querySelector('.bagSum');
-    bagSumele.innerHTML=`<div class="bag-details-container">
+    bagSumele.innerHTML = `<div class="bag-details-container">
     <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
     <div class="price-item">
       <span class="price-item-tag">Total MRP</span>
@@ -169,154 +168,156 @@ function displayBagSum(){
 }
 
 
-function loadBagItems(){
+function loadBagItems() {
     console.log(bagItems)
-    bagitemObject=bagItems.map(itemId=>{
-        for(let i=0;i<items.length;i++){
-            if(itemId==items[i].id){
-               return items[i]; 
+    bagitemObject = bagItems.map(itemId => {
+        for (let i = 0; i < items.length; i++) {
+            if (itemId == items[i].id) {
+                return items[i];
             }
         }
     });
     console.log(bagitemObject);
 }
-function displayBagItems() {
-  const items = [
-    {
-      id: "001",
-      image: "images/1.jpg",
-      company: "Carlton London",
-      item_name: "Rhodium-Plated CZ Floral Studs",
-      original_price: 1045,
-      current_price: 606,
-      discount_percentage: 42,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.5,
-        count: 1400,
-      },
-    },
-    {
-      id: "002",
-      image: "images/2.jpg",
-      company: "CUKOO",
-      item_name: "Women Padded Halter Neck Swimming Dress",
-      original_price: 2599,
-      current_price: 1507,
-      discount_percentage: 42,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.3,
-        count: 24,
-      },
-    },
-    {
-      id: "003",
-      image: "images/3.jpg",
-      company: "NUEVOSDAMAS",
-      item_name: "Women Red & White Printed A-Line Knee-Length Skirts",
-      original_price: 1599,
-      current_price: 495,
-      discount_percentage: 69,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.1,
-        count: 249,
-      },
-    },
-    {
-      id: "004",
-      image: "images/4.jpg",
-      company: "ADIDAS",
-      item_name: "Indian Cricket ODI Jersey",
-      original_price: 999,
-      current_price: 999,
-      discount_percentage: 0,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 5.0,
-        count: 10,
-      },
-    },
-    {
-      id: "005",
-      image: "images/5.jpg",
-      company: "Roadster",
-      item_name: "Pure Cotton T-shirt",
-      original_price: 1399,
-      current_price: 489,
-      discount_percentage: 65,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.2,
-        count: 3500,
-      },
-    },
-    {
-      id: "006",
-      image: "images/6.jpg",
-      company: "Nike",
-      item_name: "Men ReactX Running Shoes",
-      original_price: 14995,
-      current_price: 14995,
-      discount_percentage: 0,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 0.0,
-        count: 0,
-      },
-    },
-    {
-      id: "007",
-      image: "images/7.jpg",
-      company: "The Indian Garage Co",
-      item_name: "Men Slim Fit Regular Shorts",
-      original_price: 1599,
-      current_price: 639,
-      discount_percentage: 60,
-      rating: {
-        stars: 4.2,
-        count: 388,
-      },
-    },
-    {
-      id: "008",
-      image: "images/8.jpg",
-      company: "Nivea",
-      item_name: "Men Fresh Deodrant 150ml",
-      original_price: 285,
-      current_price: 142,
-      discount_percentage: 50,
-      return_period: 14,
-      delivery_date: "10 Oct 2023",
-      rating: {
-        stars: 4.2,
-        count: 5200,
-      },
-    },
-  ];
 
-  let containEle = document.querySelector(".bagitem-contain");
-  let innerHTML='';
-  bagitemObject.forEach(bagItem=>{
-    innerHTML+=generateItemHtml(bagItem);
-  });
-  containEle.innerHTML=innerHTML;
+function displayBagItems() {
+    const items = [{
+            id: "001",
+            image: "images/1.jpg",
+            company: "Carlton London",
+            item_name: "Rhodium-Plated CZ Floral Studs",
+            original_price: 1045,
+            current_price: 606,
+            discount_percentage: 42,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 4.5,
+                count: 1400,
+            },
+        },
+        {
+            id: "002",
+            image: "images/2.jpg",
+            company: "CUKOO",
+            item_name: "Women Padded Halter Neck Swimming Dress",
+            original_price: 2599,
+            current_price: 1507,
+            discount_percentage: 42,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 4.3,
+                count: 24,
+            },
+        },
+        {
+            id: "003",
+            image: "images/3.jpg",
+            company: "NUEVOSDAMAS",
+            item_name: "Women Red & White Printed A-Line Knee-Length Skirts",
+            original_price: 1599,
+            current_price: 495,
+            discount_percentage: 69,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 4.1,
+                count: 249,
+            },
+        },
+        {
+            id: "004",
+            image: "images/4.jpg",
+            company: "ADIDAS",
+            item_name: "Indian Cricket ODI Jersey",
+            original_price: 999,
+            current_price: 999,
+            discount_percentage: 0,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 5.0,
+                count: 10,
+            },
+        },
+        {
+            id: "005",
+            image: "images/5.jpg",
+            company: "Roadster",
+            item_name: "Pure Cotton T-shirt",
+            original_price: 1399,
+            current_price: 489,
+            discount_percentage: 65,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 4.2,
+                count: 3500,
+            },
+        },
+        {
+            id: "006",
+            image: "images/6.jpg",
+            company: "Nike",
+            item_name: "Men ReactX Running Shoes",
+            original_price: 14995,
+            current_price: 14995,
+            discount_percentage: 0,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 0.0,
+                count: 0,
+            },
+        },
+        {
+            id: "007",
+            image: "images/7.jpg",
+            company: "The Indian Garage Co",
+            item_name: "Men Slim Fit Regular Shorts",
+            original_price: 1599,
+            current_price: 639,
+            discount_percentage: 60,
+            rating: {
+                stars: 4.2,
+                count: 388,
+            },
+        },
+        {
+            id: "008",
+            image: "images/8.jpg",
+            company: "Nivea",
+            item_name: "Men Fresh Deodrant 150ml",
+            original_price: 285,
+            current_price: 142,
+            discount_percentage: 50,
+            return_period: 14,
+            delivery_date: "10 Oct 2023",
+            rating: {
+                stars: 4.2,
+                count: 5200,
+            },
+        },
+    ];
+
+    let containEle = document.querySelector(".bagitem-contain");
+    let innerHTML = '';
+    bagitemObject.forEach(bagItem => {
+        innerHTML += generateItemHtml(bagItem);
+    });
+    containEle.innerHTML = innerHTML;
 }
-function removeFromBag(itemId){
-    bagItems=bagItems.filter(bagitemId=>bagitemId!=itemId);
+
+function removeFromBag(itemId) {
+    bagItems = bagItems.filter(bagitemId => bagitemId != itemId);
     localStorage.setItem("bagItems", JSON.stringify(bagItems));
     loadBagItems();
     displayBagItems();
     disBagitem()
 }
-function generateItemHtml(item){
+
+function generateItemHtml(item) {
     return `<div class="bag-item-container">
             <div class="item-left-part">
               <img class="bag-item-img" src="${item.image}">
